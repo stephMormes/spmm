bc7ccfbf187d41b1b8dfafcc992b38e66e243a9c
Stéphane MORMES <stephane.mormes@akka.eu>
2022-07-27 08:43:35 +0200

    First commit

---
+++ src/main/java/com/sli/spmm/SpmmApplication.java
@@ -0,0 +1,13 @@
+package com.sli.spmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpmmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sli/spmm/SpmmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sli.spmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
